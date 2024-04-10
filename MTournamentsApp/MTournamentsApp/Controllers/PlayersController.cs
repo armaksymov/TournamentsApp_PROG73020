4bf5896d19ff42ef36a33b13e65a0772556d5481
Artem Maksymov <42908929+armaksymov@users.noreply.github.com>
2024-04-10 12:10:13 -0400

    Added the players (members) list page

---
+++ MTournamentsApp/MTournamentsApp/Controllers/PlayersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MTournamentsApp.Entities;
+
+namespace MTournamentsApp.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private TournamentsDbContext _tournamentsDbContext;
+
+        public PlayersController(TournamentsDbContext tournamentsDbContext)
+        {
+            _tournamentsDbContext = tournamentsDbContext;
+        }
+
+        public IActionResult List()
+        {
+            return View(GetPlayers());
+        }
+        private List<Player> GetPlayers()
+        {
+            List<Player> players = _tournamentsDbContext.Players.Include(p => p.Role).Include(p => p.Team).ToList();
+            return players;
+        }
+    }
+}
