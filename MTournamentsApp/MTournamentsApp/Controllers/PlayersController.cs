5a18bf8ec6421d6b7a6cc2193aa6132cb62f8dd6
Artem Maksymov <42908929+armaksymov@users.noreply.github.com>
2024-04-10 13:37:06 -0400

    Added the option to add players

--- MTournamentsApp/MTournamentsApp/Controllers/PlayersController.cs
+++ MTournamentsApp/MTournamentsApp/Controllers/PlayersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MTournamentsApp.Entities;
+using MTournamentsApp.Models;
 
 namespace MTournamentsApp.Controllers
 {
@@ -21,6 +22,44 @@ namespace MTournamentsApp.Controllers
         {
             List<Player> players = _tournamentsDbContext.Players.Include(p => p.Role).Include(p => p.Team).ToList();
             return players;
+        }
+
+        [HttpGet()]
+        public IActionResult Add()
+        {
+            List<PlayerRole> playerRoles = _tournamentsDbContext.PlayerRoles.OrderBy(pr => pr.PlayerRoleName).ToList();
+            List<Team> teams = _tournamentsDbContext.Teams.OrderBy(p => p.TeamName).ToList();
+
+            return View(new PlayerViewModel() { Player = new Player(), RolesList = playerRoles, TeamsList = teams });
+        }
+
+        [HttpPost()]
+        public IActionResult Add(PlayerViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                _tournamentsDbContext.Players.Add(p.Player);
+                Team? team = _tournamentsDbContext.Teams.Include(t => t.Players).Where(t => t.TeamId == p.Player.TeamId).FirstOrDefault();
+
+                if (team != null)
+                {
+                    team.PlayerIds.Add(p.Player.Id);
+                    team.Players.Add(p.Player);
+                }
+
+                _tournamentsDbContext.Teams.Update(team);//Where(t => t.TeamId == p.Player.TeamId);
+
+                _tournamentsDbContext.SaveChanges();
+
+                return RedirectToAction("List", "Players");
+            }
+            else
+            {
+                List<PlayerRole> playerRoles = _tournamentsDbContext.PlayerRoles.OrderBy(pr => pr.PlayerRoleName).ToList();
+                List<Team> teams = _tournamentsDbContext.Teams.OrderBy(p => p.TeamName).ToList();
+
+                return View(new PlayerViewModel() { Player = new Player(), RolesList = playerRoles, TeamsList = teams });
+            }
         }
     }
 }
---
+++ MTournamentsApp/MTournamentsApp/Models/PlayerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MTournamentsApp.Entities;
+
+namespace MTournamentsApp.Models
+{
+    public class PlayerViewModel
+    {
+        public ICollection<PlayerRole>? RolesList { get; set; }
+        public ICollection<Team>? TeamsList { get; set; }
+
+        public Player? Player { get; set; }
+    }
+}
+    
