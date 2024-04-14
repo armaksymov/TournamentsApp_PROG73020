097774e618fcf6a0db6e17db79ddfd1aca99486b
melbasiouny <melbasiouny@pm.me>
2024-04-14 19:47:08 -0400

    Add team creation validation

--- MTournamentsApp/MTournamentsApp/Entities/Team.cs
+++ MTournamentsApp/MTournamentsApp/Entities/Team.cs
@@ -1,13 +1,17 @@
-﻿namespace MTournamentsApp.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MTournamentsApp.Entities
 {
     public class Team
     {
         public string? TeamId { get; set; }
 
+        [Required(ErrorMessage = "Please provide the team's name")]
         public string? TeamName { get; set; }
 
         public string? TeamDescription { get; set;}
 
+        [Required(ErrorMessage = "Please select the team's game")]
         public string? MainTeamGameId { get; set; }
         public Game? MainTeamGame { get; set; }
 
