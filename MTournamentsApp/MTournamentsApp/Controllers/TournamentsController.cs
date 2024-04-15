759aa52c657ecea1ae78fa0aab1f4cffb4427773
Artem Maksymov <42908929+armaksymov@users.noreply.github.com>
2024-04-14 23:31:15 -0400

    Added option to add tournaments

--- MTournamentsApp/MTournamentsApp/Controllers/TournamentsController.cs
+++ MTournamentsApp/MTournamentsApp/Controllers/TournamentsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MTournamentsApp.Entities;
+using MTournamentsApp.Models;
 using System.IO;
 
 namespace MTournamentsApp.Controllers
@@ -24,10 +26,76 @@ namespace MTournamentsApp.Controllers
 
             foreach (var tournament in tournaments)
             {
-                tournament.TournamentTeams = _tournamentsDbContext.Teams.Where(t => tournament.TeamIds.Contains(t.TeamId)).OrderBy(t => t.TeamId).ToList();
+                tournament.TournamentTeams = _tournamentsDbContext.Teams.Where(t => t.TournamentIds.Contains(tournament.Id)).OrderBy(t => t.TeamId).ToList();
             }
 
             return tournaments;
+        }
+
+        [HttpGet()]
+        public IActionResult Add()
+        {
+            List<Game> games = _tournamentsDbContext.Games.OrderBy(g => g.GameName).ToList();
+            List<Team> teams = _tournamentsDbContext.Teams.OrderBy(t => t.TeamName).ToList();
+            Tournament newTournament = new Tournament() { Address = new Address() };
+
+            return View(new TournamentViewModel() { Tournament = newTournament, GamesList = games, TeamsList = teams });
+        }
+
+        [HttpPost()]
+        public IActionResult Add(TournamentViewModel tvm, List<string> SelectedTeamIds)
+        {
+            if (!ModelState.IsValid || SelectedTeamIds.Count < 2)
+            {
+                if (SelectedTeamIds.Count < 2)
+                {
+                    ModelState.AddModelError("SelectedTeamIds", "Please select at least two teams for the tournament.");
+                }
+                return View(tvm);
+            }
+
+            Address? address = _tournamentsDbContext.Addresses.Where(a => a.StreetAddress == tvm.Tournament.Address.StreetAddress && a.TournamentCity == tvm.Tournament.Address.TournamentCity && a.TournamentCountry == tvm.Tournament.Address.TournamentCountry).FirstOrDefault();
+            Address customAddress;
+            if (address == null)
+            {
+                customAddress = new Address() { StreetAddress = tvm.Tournament.Address.StreetAddress, TournamentCity = tvm.Tournament.Address.TournamentCity, TournamentCountry = tvm.Tournament.Address.TournamentCountry, TournamentPostalCode = tvm.Tournament.Address.TournamentPostalCode };
+                _tournamentsDbContext.Addresses.Add(customAddress);
+
+                tvm.Tournament.Address = customAddress;
+                tvm.Tournament.AddressID = customAddress.Id;
+            }
+            else
+            {
+                tvm.Tournament.AddressID = address.Id;
+                tvm.Tournament.Address = address;
+            }
+
+            _tournamentsDbContext.Tournaments.Add(tvm.Tournament);
+            _tournamentsDbContext.SaveChanges();
+
+
+            tvm.Tournament.TeamIds = SelectedTeamIds;
+
+            List<Team> teams = _tournamentsDbContext.Teams.Where(t => SelectedTeamIds.Contains(t.TeamId)).ToList(); 
+
+            foreach (var team in teams)
+            {
+                if(team.TournamentIds.IsNullOrEmpty())
+                {
+                    team.TournamentIds = new List<int>();
+                }
+                if (team.Tournaments.IsNullOrEmpty())
+                {
+                    team.Tournaments = new List<Tournament>();
+                }
+                team.TournamentIds.Add(tvm.Tournament.Id);
+                team.Tournaments.Add(tvm.Tournament);
+                _tournamentsDbContext.Teams.Update(team);
+            }
+
+            _tournamentsDbContext.SaveChanges();
+
+            return RedirectToAction("List", "Tournaments");
         }
     }
 }
---
+++ MTournamentsApp/MTournamentsApp/Models/TournamentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MTournamentsApp.Entities;
+
+namespace MTournamentsApp.Models
+{
+    public class TournamentViewModel
+    {
+        public ICollection<Game>? GamesList { get; set; }
+        public ICollection<Team>? TeamsList { get; set; }
+        public Tournament? Tournament { get; set; }
+    }
+}
