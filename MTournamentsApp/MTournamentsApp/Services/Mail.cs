339653198dae04a22a20cb3bb5c28c217feb82fe
melbasiouny <melbasiouny@pm.me>
2024-04-17 14:09:39 -0400

    Add different email variants

--- MTournamentsApp/MTournamentsApp/Services/Mail.cs
+++ MTournamentsApp/MTournamentsApp/Services/Mail.cs
@@ -1,18 +1,21 @@
 ﻿using MTournamentsApp.Entities;
 using System.Net.Mail;
 using System.Net;
+using System.IO;
 
 namespace MTournamentsApp.Services
 {
     public class Mail(IHttpContextAccessor httpContextAccessor) : IMail
     {
+        const string fromAddress = "mtournamentz@outlook.com";
+        const string fromAddressPassword = "bYRc2I/71]5y";
         public bool SendInvite(Tournament tournament, Invitation recipient)
         {
+            var invitationUrl =
+                $"{httpContextAccessor.HttpContext?.Request.Scheme}://{httpContextAccessor.HttpContext?.Request.Host.ToUriComponent()}/Tournaments/{tournament.Id}/Invitations/{recipient.InvitationId}";
+
             try
             {
-                const string fromAddress = "mtournamentz@outlook.com";
-                const string fromAddressPassword = "bYRc2I/71]5y";
-
                 var smtpClient = new SmtpClient("smtp-mail.outlook.com")
                 {
                     Port = 587,
@@ -22,15 +25,16 @@ namespace MTournamentsApp.Services
                     EnableSsl = true
                 };
 
-                var invitationUrl =
-                    $"{httpContextAccessor.HttpContext?.Request.Scheme}://{httpContextAccessor.HttpContext?.Request.Host.ToUriComponent()}/tournament/{tournament.Id}/invitations/{recipient.InvitationId}";
-
-                // Create mail message
                 var mailMessage = new MailMessage
                 {
                     From = new MailAddress(fromAddress),
                     Subject = $"You have been invited to the \"{tournament.TournamentName}\" tournament!",
-                    Body = $"",
+                    Body = $"" +
+                    $"<h1>Hello {recipient.PlayerName}:</h1>" +
+                    $"<p>You have been invited to the \"{tournament.TournamentName}\" at {tournament.Address.ToString()} on {tournament.TournamentDate}!</p>" +
+                    $"<p>We would be thrilled to have you so please <a href=\"{invitationUrl}\">let us know</a> if you can as soon as possible!</p>" +
+                    $"<p>Sincerely,</p>" +
+                    $"<p>The MTournamentZ Team</p>",
                     IsBodyHtml = true
                 };
 
@@ -49,9 +53,6 @@ namespace MTournamentsApp.Services
         {
             try
             {
-                const string fromAddress = "mtournamentz@outlook.com";
-                const string fromAddressPassword = "bYRc2I/71]5y";
-
                 var smtpClient = new SmtpClient("smtp-mail.outlook.com")
                 {
                     Port = 587,
@@ -61,7 +62,6 @@ namespace MTournamentsApp.Services
                     EnableSsl = true
                 };
 
-                // Create mail message
                 var mailMessage = new MailMessage
                 {
                     From = new MailAddress(fromAddress),
@@ -69,10 +69,46 @@ namespace MTournamentsApp.Services
                     Body = $"<p>Dear {recipient.FirstName},</p>" +
                    $"<p>We regret to inform you that the \"{tournament.TournamentName}\" tournament scheduled for {tournament.TournamentDate?.ToString("dddd, MMMM dd, yyyy")} has been cancelled due to unforeseen circumstances.</p>" +
                    $"<p>We understand this may be disappointing and apologize for any inconvenience this may cause. We value your participation and look forward to seeing you at future tournaments.</p>" +
-                   $"<p>If you have any questions or need further information, please feel free to contact us at any time.</p>" +
                    $"<p>Thank you for your understanding.</p>" +
                    $"<p>Sincerely,</p>" +
                    $"<p>The MTournamentZ Team</p>",
+                    IsBodyHtml = true
+                };
+
+                mailMessage.To.Add(recipient.Email);
+                smtpClient.Send(mailMessage);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool SendTeamKick(Tournament tournament, Player recipient)
+        {
+            try
+            {
+                var smtpClient = new SmtpClient("smtp-mail.outlook.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(fromAddress, fromAddressPassword),
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    EnableSsl = true
+                };
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(fromAddress),
+                    Subject = $"Team Disqualification from the \"{tournament.TournamentName}\" Tournament",
+                    Body = $"<p>Dear {recipient.FirstName},</p>" +
+                    $"<p>We regret to inform you that your team has been removed from the \"{tournament.TournamentName}\" tournament scheduled for {tournament.TournamentDate?.ToString("dddd, MMMM dd, yyyy")} due to non-compliance with the tournament regulations.</p>" +
+                    $"<p>This decision was made to maintain the integrity and fairness of the competition. We appreciate your past participation and hope this situation will be a learning opportunity.</p>" +
+                    $"<p>Thank you for your understanding.</p>" +
+                    $"<p>Sincerely,</p>" +
+                    $"<p>The MTournamentZ Team</p>",
                     IsBodyHtml = true
                 };
 
